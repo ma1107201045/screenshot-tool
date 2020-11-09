9b8f9866ee0180fe9c58abf364f2debd9bddf483
global_mwy <global_1107201045@qq.com>
2020-11-09 20:07:09 +0800

    Main.java

--- src/com/lingyi/Main.java
+++ src/com/lingyi/Main.java
@@ -1,6 +1,7 @@
 package com.lingyi;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
